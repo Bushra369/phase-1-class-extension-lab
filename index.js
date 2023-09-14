ff80a359b6211a1f8f86976c3eda492f24a87702
BouchraDjalti <boucradjalti369@gmail.com>
2023-09-14 15:07:27 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((acc, side) => acc + side, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  
+
+  get isValid() {
+    if (this.countSides !== 3) return false;
+
+    const [a, b, c] = this.sides;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+
+  get isValid() {
+    if (this.countSides !== 4) return false;
+
+    return this.sides.every(side => side === this.sides[0]);
+  }
+
+  get area() {
+    if (this.isValid) {
+      return this.sides[0] ** 2;
+    } else {
+      return "Invalid square: All sides must have equal length.";
+    }
+  }
+}
+
+
